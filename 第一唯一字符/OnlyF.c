7aa4ad286ad5ee56b04481dd5fbccb93bb8a6f54
yangwberic <yangwberic@126.com>
2019-08-21 16:06:16 +0800

    给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在返回-1

---
+++ 第一唯一字符/OnlyF.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int OnlyFirst(char* ch, int size ) {
+	int i;
+	char poss[256] = { 0 };
+	for (i = 0; i < size; i++) {
+		poss[ch[i]]++;
+	}
+	for (i = 0; i < size; i++) {
+		if (poss[ch[i]]==1) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	char ch[100];
+	gets(ch);
+	printf("%d\n", OnlyFirst(ch,sizeof(ch)));
+	system("pause");
+	return 0;
+}
